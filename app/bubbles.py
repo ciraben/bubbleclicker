47d427edc74b165198c4e07117596c0b4bcb92b9
LighthouseRobin <didiongit@gmail.com>
2025-01-26 15:43:39 -0800

    Create class for each bubble type

--- app/bubbles.py
+++ app/bubbles.py
@@ -5,3 +5,15 @@ class BasicBubble(arcade.Sprite):
 
     def __init__(self, *args):
         super().__init__(path_or_texture=self.ART_PATH, *args)
+
+class MilkBubble(BasicBubble):
+    ART_PATH = "art/milkbubble.png"
+
+class SlimeBubble(BasicBubble):
+    ART_PATH = "art/slimebubble.png"
+
+class RoyalJellyBubble(BasicBubble):
+    ART_PATH = "art/royaljellybubble.png"
+
+class MagmaBubble(BasicBubble):
+    ART_PATH = "art/magmabubble.png"
