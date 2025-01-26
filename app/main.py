33b8f0a2e9304b549dcddfa2d8741801a1db861d
ciraben <tom.on.github@gmail.com>
2025-01-26 10:42:40 -0800

    Create a base view subclass for us to modify

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3.9
 
 import arcade
+from views import BaseView
 
 def main():
     window = arcade.Window()
+    view = BaseView()
+    view.setup()
+    window.show_view(view)
     arcade.run()
 
 if __name__ == "__main__":
