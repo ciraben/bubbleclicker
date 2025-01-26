1d64a9c56e698046799de3c78d6f47bb59127d20
ciraben <tom.on.github@gmail.com>
2025-01-26 10:46:26 -0800

    Draw a bubble sprite on screen

--- app/views.py
+++ app/views.py
@@ -2,4 +2,15 @@ import arcade
 
 class BaseView(arcade.View):
     def setup(self):
-        pass
+        self.bubbles = arcade.SpriteList()
+
+        new_bubble = arcade.Sprite("art/mainbubble.png")
+        new_bubble.position = self.center
+        self.bubbles.append(new_bubble)
+
+    def on_update(self, dt):
+        self.bubbles.update(dt)
+
+    def on_draw(self):
+        self.clear()
+        self.bubbles.draw()
