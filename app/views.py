95cffe64c1201e6d775a3a6eeb237b4e7dc9dc4f
ciraben <tom.on.github@gmail.com>
2025-01-26 16:18:10 -0800

    Increment counter on bubble pop

--- app/views.py
+++ app/views.py
@@ -19,13 +19,22 @@ class BaseView(arcade.gui.view.UIView):
         points_sidebar = arcade.gui.UIBoxLayout(align="right")
         ui_layout.add(points_sidebar, anchor_x="right", anchor_y="top")
 
-        self.points_label = arcade.gui.UILabel(text = "0 BP")
-        self.points_label_2 = arcade.gui.UILabel(text = "343 BP")
-        points_sidebar.add(self.points_label)
-        points_sidebar.add(self.points_label_2)
+        self.bubble_points = 0
+        self.BP_label = arcade.gui.UILabel(text = "0 BP")
+        points_sidebar.add(self.BP_label)
 
     def on_update(self, dt):
         self.bubbles.update(dt)
 
     def on_draw_before_ui(self):
         self.bubbles.draw()
+
+    def on_mouse_press(self, x, y, button, mods):
+        if button != arcade.MOUSE_BUTTON_LEFT:
+            return
+
+        clicked_bubbles = arcade.get_sprites_at_point((x, y), self.bubbles)
+        for bubble in clicked_bubbles:
+            bubble.remove_from_sprite_lists()
+            self.bubble_points += 1
+        self.BP_label.text = f"{self.bubble_points} BP"
