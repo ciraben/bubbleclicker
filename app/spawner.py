42d0af9980becadf88e4c0080a3eae7689ad40e6
ciraben <tom.on.github@gmail.com>
2025-01-26 16:45:34 -0800

    Spawn a bubble per second

--- app/spawner.py
+++ app/spawner.py
@@ -4,11 +4,18 @@ from bubbles import BasicBubble
 class Spawner():
     def __init__(self, view):
         self.view = view
+        self.elapsed_time_since_last_bubble = 0
 
     def setup(self):
+        self.spawn_soap_bubble()
+
+    def spawn_things(self, dt):
+        self.elapsed_time_since_last_bubble += dt
+        if self.elapsed_time_since_last_bubble > 1:
+            self.spawn_soap_bubble()
+            self.elapsed_time_since_last_bubble = 0
+
+    def spawn_soap_bubble(self):
         new_bubble = BasicBubble()
         new_bubble.position = self.view.center
         self.view.bubbles.append(new_bubble)
-
-    def spawn_things(self, dt):
-        pass
