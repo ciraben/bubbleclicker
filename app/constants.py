8d7de7ac4583e3ac82e4b93873967cf25f18abb2
ciraben <tom.on.github@gmail.com>
2025-01-27 14:43:29 -0800

    Add global padding to UI

--- app/constants.py
+++ app/constants.py
@@ -3,6 +3,7 @@ import arcade
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 800
 WINDOW_TITLE = "bubble clicker"
+
 BASE_SPAWN_RATE = 3
 BASE_POINT_VALUE = 1
 
@@ -11,3 +12,6 @@ bubblepopsound = (
     arcade.load_sound('audio/bubblepop2.mp3'),
     arcade.load_sound('audio/bubblepop3.mp3')
 )
+
+# UI
+UI_GLOBAL_PADDING = 20
